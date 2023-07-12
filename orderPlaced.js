29fd58fe9e88a29cf69e09cfa3a43157cfb46ef7
daovietanh190499 <daovietanhsky@gmail.com>
2023-07-12 20:53:26 +0700

    remove

--- orderPlaced.js
+++
@@ -1,6 +0,0 @@
-fetch(`https://script.google.com/macros/s/AKfycbw7JpGnyfg5JjmA-jaEKX5X-bs87xyEm0j4EsYgMor7a4BUyDHuU94JRduU1UJAoSvJ/exec?name=${"vietanh"}&phone=${"0377669361"}&email=${"daovietanhsky@gmail.com"}&address=${"Từ Hồ, Hưng Yên"}&order_content=${window.localStorage.getItem("cart_infor")}`)
-.then(res => res.json())
-.then(res => {
-    console.log(res)
-    window.localStorage.setItem("cart_infor", JSON.stringify({"list": []}))
-})
--- routes.js
+++ routes.js
@@ -53,6 +53,14 @@ const appVue = new Vue({
             }
             window.localStorage.setItem("cart_infor", JSON.stringify(cart_infor))
             this.cart_infor = cart_infor
+        },
+        orderPlace() {
+            fetch(`https://script.google.com/macros/s/AKfycbw7JpGnyfg5JjmA-jaEKX5X-bs87xyEm0j4EsYgMor7a4BUyDHuU94JRduU1UJAoSvJ/exec?name=${"vietanh"}&phone=${"0377669361"}&email=${"daovietanhsky@gmail.com"}&address=${"Từ Hồ, Hưng Yên"}&order_content=${window.localStorage.getItem("cart_infor")}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                window.localStorage.setItem("cart_infor", JSON.stringify({"list": []}))
+            })
         }
     }
 })
