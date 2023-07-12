8deb87f97028ee375de0ed843d90cb566690ba2b
daovietanh190499 <daovietanhsky@gmail.com>
2023-07-12 21:03:33 +0700

    form

--- routes.js
+++ routes.js
@@ -54,7 +54,7 @@ const appVue = new Vue({
             window.localStorage.setItem("cart_infor", JSON.stringify(cart_infor))
             this.cart_infor = cart_infor
         },
-        orderPlace() {
+        placeOrder() {
             fetch(`https://script.google.com/macros/s/AKfycbw7JpGnyfg5JjmA-jaEKX5X-bs87xyEm0j4EsYgMor7a4BUyDHuU94JRduU1UJAoSvJ/exec?name=${"vietanh"}&phone=${"0377669361"}&email=${"daovietanhsky@gmail.com"}&address=${"Từ Hồ, Hưng Yên"}&order_content=${window.localStorage.getItem("cart_infor")}`)
             .then(res => res.json())
             .then(res => {
